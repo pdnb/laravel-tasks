ca1f425cf09be3a9a67d27b3076ab12ec2fcc15f
Keerati Intarurk <iampdnb@gmail.com>
2023-06-27 11:27:29 +0700

    Configure DB & redis variables

--- config/database.php
+++ config/database.php
@@ -138,7 +138,7 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
+            'url' => env('AZURE_REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('AZURE_REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
